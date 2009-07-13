41a0e484e3e9342c909b512a814f91c2b792ad6c
Frédéric Bertolus <fred.bertolus@gmail.com>
2009-07-13 18:21:35 +0000

    Add licence

--- Makefile
+++ Makefile
@@ -1,11 +1,18 @@
 BUILD_DIR=out
 TARGET=$(BUILD_DIR)/index.php
-SOURCES=$(wildcard src/shrew.php) \
+SOURCES=\
+$(wildcard src/licence.php) \
+$(wildcard src/shrew.php) \
 $(wildcard src/main.php)
+PHP_PREFIX="<?php"
+PHP_SUFFIX="?>"
 
 all:
 	@mkdir -p $(BUILD_DIR)
 	@rm -f $(TARGET)
 	@touch $(TARGET)
-	@cat $(SOURCES) >> $(TARGET)
+	@echo $(PHP_PREFIX) >> $(TARGET)
+	@cat $(SOURCES) |grep -v "<?php" | grep -v "?>" | grep -v "//l" >> $(TARGET)
+	
+	@echo $(PHP_SUFFIX) >> $(TARGET)
 	@echo "Build successful :" $(TARGET)
--- src/licence.php
+++ src/licence.php
@@ -16,38 +16,19 @@
 //l   You should have received a copy of the GNU Affero General Public License
 //l   along with Shrew-gallery.  If not, see <http://www.gnu.org/licenses/>.
 
-class Shrew {
-  function Run()
-  {
-  	//Load config
-    $config = new Config();
-    $config->Load();
-    
-    $displayManager = new DisplayManager();
-	  
-    //Create Login Manager
-    $loginManager = new LoginManager();
-    
-    if($loginManager->WantLogin())
-    {
-      $loginManager->TryLogin();
-    }
-    
-    
-    if($loginManager->IsLogged())
-    {
-      $imageManager = new ImageManager();
-      $images = $imageManager->GetImages();
-      
-      $displayManager->DisplayImagesPage($images);
-      
-    }
-    else
-    {
-      $displayManager->DisplayLoginPage();
-    }
-    
-  }
-}
-
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 ?>
